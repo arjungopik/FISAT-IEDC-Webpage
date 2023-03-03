9ededb75ae04f69dbbbc2f7fb3c68308f0bab8bc
arjungopik <arjungopik111@gmail.com>
2023-03-03 12:12:23 +0530

    tt

--- js/main.js
+++ js/main.js
@@ -2,14 +2,7 @@
     "use strict";
 
     // Spinner
-    var spinner = function () {
-        setTimeout(function () {
-            if ($('#spinner').length > 0) {
-                $('#spinner').removeClass('show');
-            }
-        }, 1);
-    };
-    spinner();
+   
     
     
     // Initiate the wowjs
